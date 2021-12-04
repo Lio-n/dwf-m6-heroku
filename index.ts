dc90e0c9658bfc911f9f8a515559947660410420
Lion <leonardofontan14@gmail.com>
2021-12-04 19:28:50 -0300

    Second Commit

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import * as express from "express";
 const app = express();
 
 app.get("/hola", (req, res) => {
-  res.json(`Todo Ok`);
+  res.json(`Todo Ok, on port 3000`);
 });
 
 app.listen(3000, () => {
